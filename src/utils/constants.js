6e471825740c9c2d14adb9fb44eec70b200eeda2
Vijay Raghuwanshi <ervijayraghuwanshi@gmail.com>
2026-02-16 23:41:50 +0530

    refactor(constants): update default host to docker-livy service
    - Changed default host name from "Localhost" to "docker-livy"
    - Updated default URL from "http://localhost:8998" to "http://livy:8998" for Docker Compose networking

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const DEFAULT_HOST = {
   id: "default",
-  name: "Localhost",
-  url: "http://localhost:8998",
+  name: "docker-livy",
+  url: "http://livy:8998",
 };
 
 export const SESSION_KIND = "sql";
